7e2c472b2db4c2be92c2a2cbacf6cb489ee44b62
maticzav <matic.zavadlal@gmail.com>
2020-12-01 10:20:27 +0100

    :sparkles: template

--- README.md
+++ README.md
@@ -1,6 +1,51 @@
 # advent-of-code-2020
 Rešitve Advent of Code 2020 v OCaml-u
 
+## Template
+
+```ocaml
+(* Nastavitve *)
+
+let dan_naloge = "1"
+
+(* Naloge *)
+
+let naloga1 vsebina_datoteke = "10"
+
+let naloga2 vsebina_datoteke = string_of_int (String.length vsebina_datoteke)
+
+(* Pomožne funkcije *)
+
+let preberi_datoteko ime_datoteke =
+  let chan = open_in ime_datoteke in
+  let vsebina = really_input_string chan (in_channel_length chan) in
+  close_in chan;
+  vsebina
+
+let izpisi_datoteko ime_datoteke vsebina =
+  let chan = open_out ime_datoteke in
+  output_string chan vsebina;
+  close_out chan
+
+let ime_vhodne_datoteke dan = "day_" ^ dan ^ ".in"
+
+let ime_izhodne_datoteke dan naloga = "day_" ^ dan ^ "_" ^ naloga ^ ".out"
+
+(* Main *)
+
+let _ =
+  (* Preberi podatke *)
+  let vsebina_datoteke = preberi_datoteko (ime_vhodne_datoteke dan_naloge) in
+  (* Izračunaj rešitev *)
+  let odgovor1 = naloga1 vsebina_datoteke in
+  let odgovor2 = naloga2 vsebina_datoteke in
+  (* Izpiši podatke *)
+  izpisi_datoteko (ime_izhodne_datoteke dan_naloge "1") odgovor1;
+  izpisi_datoteko (ime_izhodne_datoteke dan_naloge "2") odgovor2
+```
+
+
+
 ## Nastavitev razvijalskega okolja
 
 Repository uporabla VSCode Docker container za delovanje, zato mora Docker biti zagnan v ozadju vedno, ko urejamo kodo.
--- day_1.ml
+++ day_1.ml
@@ -1,23 +1,38 @@
-let naloga1 vsebina_datoteke =
-    "10"
+(* Nastavitve *)
 
-let naloga2 vsebina_datoteke =
-    string_of_int (String.length vsebina_datoteke)
+let dan_naloge = "1"
+
+(* Naloge *)
+
+let naloga1 vsebina_datoteke = "10"
+
+let naloga2 vsebina_datoteke = string_of_int (String.length vsebina_datoteke)
+
+(* Pomožne funkcije *)
+
+let preberi_datoteko ime_datoteke =
+  let chan = open_in ime_datoteke in
+  let vsebina = really_input_string chan (in_channel_length chan) in
+  close_in chan;
+  vsebina
+
+let izpisi_datoteko ime_datoteke vsebina =
+  let chan = open_out ime_datoteke in
+  output_string chan vsebina;
+  close_out chan
+
+let ime_vhodne_datoteke dan = "day_" ^ dan ^ ".in"
+
+let ime_izhodne_datoteke dan naloga = "day_" ^ dan ^ "_" ^ naloga ^ ".out"
+
+(* Main *)
 
 let _ =
-    let preberi_datoteko ime_datoteke =
-        let chan = open_in ime_datoteke in
-        let vsebina = really_input_string chan (in_channel_length chan) in
-        close_in chan;
-        vsebina
-    and izpisi_datoteko ime_datoteke vsebina =
-        let chan = open_out ime_datoteke in
-        output_string chan vsebina;
-        close_out chan
-    in
-    let vsebina_datoteke = preberi_datoteko "day_0.in" in
-    let odgovor1 = naloga1 vsebina_datoteke
-    and odgovor2 = naloga2 vsebina_datoteke
-    in
-    izpisi_datoteko "day_0_1.out" odgovor1;
-    izpisi_datoteko "day_0_2.out" odgovor2
+  (* Preberi podatke *)
+  let vsebina_datoteke = preberi_datoteko (ime_vhodne_datoteke dan_naloge) in
+  (* Izračunaj rešitev *)
+  let odgovor1 = naloga1 vsebina_datoteke in
+  let odgovor2 = naloga2 vsebina_datoteke in
+  (* Izpiši podatke *)
+  izpisi_datoteko (ime_izhodne_datoteke dan_naloge "1") odgovor1;
+  izpisi_datoteko (ime_izhodne_datoteke dan_naloge "2") odgovor2
